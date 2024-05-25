cdab88d379fa946acdda5474a9e46babb669ba6d
miro <miro@elbe5.de>
2024-05-25 13:40:26 +0200

    fix

--- Sources/E5Data/CGRect.swift
+++ Sources/E5Data/CGRect.swift
@@ -8,15 +8,15 @@ import Foundation
 
 extension CGRect{
     
-    func scaleBy(_ scale: CGFloat) -> CGRect{
+    public func scaleBy(_ scale: CGFloat) -> CGRect{
         CGRect(origin: CGPoint(x: origin.x*scale, y: origin.y*scale), size: CGSize(width: size.width*scale, height: size.height*scale))
     }
     
-    func scaleCenteredBy(_ scale: CGFloat) -> CGRect{
+    public func scaleCenteredBy(_ scale: CGFloat) -> CGRect{
         CGRect(origin: CGPoint(x: origin.x + size.width/2 - size.width*scale/2, y: origin.y + size.height/2 - size.height*scale/2), size: CGSize(width: size.width*scale, height: size.height*scale))
     }
     
-    func expandBy(size: CGSize) -> CGRect{
+    public func expandBy(size: CGSize) -> CGRect{
         CGRect(origin: CGPoint(x: origin.x - size.width, y: origin.y - size.height), size: CGSize(width: size.width + 2*size.width, height: size.height + 2*size.height))
     }
     
