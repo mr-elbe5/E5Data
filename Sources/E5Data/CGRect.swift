d312edda71dee074572e7e66c9a6a2b1cc44afa0
miro <miro@elbe5.de>
2024-05-25 13:39:12 +0200

    cgrect

---
+++ Sources/E5Data/CGRect.swift
@@ -0,0 +1,23 @@
+/*
+ Maps For OSM
+ App for display and use of OSM maps without MapKit
+ Copyright: Michael Rönnau mr@elbe5.de
+ */
+
+import Foundation
+
+extension CGRect{
+    
+    func scaleBy(_ scale: CGFloat) -> CGRect{
+        CGRect(origin: CGPoint(x: origin.x*scale, y: origin.y*scale), size: CGSize(width: size.width*scale, height: size.height*scale))
+    }
+    
+    func scaleCenteredBy(_ scale: CGFloat) -> CGRect{
+        CGRect(origin: CGPoint(x: origin.x + size.width/2 - size.width*scale/2, y: origin.y + size.height/2 - size.height*scale/2), size: CGSize(width: size.width*scale, height: size.height*scale))
+    }
+    
+    func expandBy(size: CGSize) -> CGRect{
+        CGRect(origin: CGPoint(x: origin.x - size.width, y: origin.y - size.height), size: CGSize(width: size.width + 2*size.width, height: size.height + 2*size.height))
+    }
+    
+}
