779fe8873c41c5895fedf163331d1436eaaaa60c
miro <mr@elbe5.de>
2024-07-14 16:49:14 +0200

    loglevels, cache

--- Sources/E5Data/Log.swift
+++ Sources/E5Data/Log.swift
@@ -8,36 +8,67 @@ import Foundation
 
 public class Log{
     
+    public enum LogLevel: Int{
+        case none
+        case error
+        case warn
+        case info
+        case debug
+    }
+    
+    public static var cache = Array<String>()
+    
+    public static var useCache = false
+    
+    public static var logLevel: LogLevel = .error
+    
     private static func log(_ str: String){
         print(str)
+        if useCache{
+            cache.append(str)
+        }
     }
 
     public static func debug(_ msg: String){
-        log("debug: \(msg)")
+        if logLevel.rawValue >= LogLevel.debug.rawValue{
+            log("debug: \(msg)")
+        }
     }
 
     public static func info(_ msg: String){
-        log("info: \(msg)")
+        if logLevel.rawValue >= LogLevel.info.rawValue{
+            log("info: \(msg)")
+        }
     }
 
     public static func warn(_ msg: String){
-        log("warn: \(msg)")
+        if logLevel.rawValue >= LogLevel.warn.rawValue{
+            log("warn: \(msg)")
+        }
     }
 
     public static func error(_ msg: String){
-        log("error: \(msg)")
+        if logLevel.rawValue >= LogLevel.error.rawValue{
+            log("error: \(msg)")
+        }
     }
 
     public static func error(_ msg: String, error: Error){
-        log("error: \(msg): \(error.localizedDescription)")
+        if logLevel.rawValue >= LogLevel.error.rawValue{
+            log("error: \(msg): \(error.localizedDescription)")
+        }
     }
 
     public static func error(error: Error){
-        log("error: \(error.localizedDescription)")
+        if logLevel.rawValue >= LogLevel.error.rawValue{
+            log("error: \(error.localizedDescription)")
+        }
     }
 
     public static func error(msg: String, error: Error){
-        log("error: \(msg): \(error.localizedDescription)")
+        if logLevel.rawValue >= LogLevel.error.rawValue{
+            log("error: \(msg): \(error.localizedDescription)")
+        }
     }
     
 }
